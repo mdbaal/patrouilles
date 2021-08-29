e1e927356baf634872b68ebc9d1a712320743e79
mirco <mircobaalmans@live.nl>
2021-08-29 10:33:51 +0200

    Moved functions to controller

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import time
 import json
-from ScoutController import ScoutController
-from PatrouilleController import PatrouilleController
-from PatrouilleGenerator import PatrouilleGenerator
+
+from AppController import AppController
+from PatrouilleGen import *
+
 import random
+from UI.UIApp import App
 
 scoutController = ScoutController()
 patrouilleController = PatrouilleController()
 patrouilleGenerator = PatrouilleGenerator()
+appControl = AppController()
 
 if __name__ == '__main__':
     random.seed(time.time())
-
+    appControl.Setup(scoutController,patrouilleController, patrouilleGenerator)
+    print(appControl)
     with open('data/scouts.json') as scoutsJson:
         scoutData = json.load(scoutsJson)
 
@@ -19,5 +23,9 @@ if __name__ == '__main__':
         scoutController.NewScout(s["Naam"], s["Leeftijd"], s["Insignelevel"], title=s["Title"])
 
     patrouilleGenerator.GeneratePatrouilles3(patrouilleController, scoutController, ["Beren", "Vossen", "Panters"])
-    print(patrouilleController.printPatrouilles())
+    # print(patrouilleController.printPatrouilles())
+
+    app = App()
+
+    app.mainloop()
 
