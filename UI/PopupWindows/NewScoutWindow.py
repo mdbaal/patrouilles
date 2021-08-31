3e9b9de03202b26d38bf1659e52f32f1fc784b1f
mirco <mircobaalmans@live.nl>
2021-08-31 14:25:41 +0200

    Added base popupwindow class

---
+++ UI/PopupWindows/NewScoutWindow.py
@@ -0,0 +1,57 @@
+from tkinter import *
+from tkinter.ttk import Combobox
+from .PopupWindow import PopupWindow
+
+
+class NewScoutWindow(PopupWindow):
+    def __init__(self, master, title, bg="lightgrey", submit_command=None):
+        super().__init__(master, bg=bg, title=title, submit_command=submit_command)
+        self.geometry("400x200")
+
+        Grid.columnconfigure(self, 0, weight=1
+                             )
+        Grid.columnconfigure(self, 1, weight=1)
+
+        # Edit Name
+        self.labelName: Label = Label(self, text="Naam")
+        self.labelName.grid(row=0, column=0, sticky="EW", padx=20, pady=10)
+
+        self.entryName: Entry = Entry(self, textvariable=self._input["Naam"])
+        self.entryName.grid(row=1, column=0, sticky="EW", padx=20, pady=5)
+        self.entryName.focus()
+
+        # Edit Leeftijd
+        self.labelAge: Label = Label(self, text="Leeftijd")
+        self.labelAge.grid(row=0, column=1, sticky="EW", padx=20, pady=10)
+
+        self.entryAge: Spinbox = Spinbox(self, from_=11, to=16, textvariable=self._input["Age"])
+        self.entryAge.grid(row=1, column=1, sticky="EW", padx=20, pady=5)
+
+        # Edit Insigne
+        self.labelInsigne: Label = Label(self, text="Insigne")
+        self.labelInsigne.grid(row=2, column=0, sticky="EW", padx=20, pady=5)
+
+        self.entryInsigne: Spinbox = Spinbox(self, from_=0, to=5, textvariable=self._input["Insigne"])
+        self.entryInsigne.grid(row=3, column=0, sticky="EW", padx=20, pady=5)
+
+        # Edit Relations
+        self.labelRelations: Label = Label(self, text="Relaties")
+        self.labelRelations.grid(row=2, column=1, sticky="EW", padx=20, pady=5)
+
+        # TODO Test relations with menu buttons
+        self.entryRelation: Combobox = Combobox(self, textvariable=self._input["Relations"])
+        self.entryRelation.grid(row=3, column=1, sticky="EW", padx=20, pady=5)
+
+        # Edit Title
+        self.labelTitle: Label = Label(self, text="Title")
+        self.labelTitle.grid(row=4, column=0, sticky="EW", padx=20, pady=5)
+        self.entryTitle: Combobox = Combobox(self, textvariable=self._input["Title"])
+        self.entryTitle['values'] = ('Lid', 'Pl', 'Apl')
+        self.entryTitle.set('Lid')
+        self.entryTitle['state'] = 'readonly'
+        self.entryTitle.grid(row=5, column=0, sticky="EW", padx=20, pady=5)
+
+        # Confirm
+        self.button = Button(self, text="Submit", command=self.submit)
+        self.button.grid(row=6, columnspan=2, sticky="EW", padx=20, pady=10)
+
---
+++ UI/PopupWindows/PopupWindow.py
@@ -0,0 +1,24 @@
+from tkinter import *
+from typing import Dict
+
+
+class PopupWindow(Toplevel):
+
+    def __init__(self, title=None, master=None, bg="lightgrey", submit_command=None):
+        super().__init__(self, master, bg=bg)
+
+        if title is not None:
+            self.title(title)
+
+        self._submit_command = submit_command
+        self._input: Dict = {}
+
+    # Submit data from window to function
+    def submit(self):
+        if self._submit_command is not None:
+            self._submit_command(self._input)
+        self.withdraw()
+
+    # To get the input after window is closed
+    def GetInput(self):
+        return self._input
